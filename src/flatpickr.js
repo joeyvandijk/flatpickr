35683fcfe70e5620674ad711c3ab90ad29daa267
Joey van Dijk <contact@joeyvandijk.nl>
2016-08-15 14:10:04 +0200

    fix issue when viewing last month of the year and selecting next (or previous) day of another month. Next month-day will result in 2018 while expecting 2017

--- src/flatpickr.js
+++ src/flatpickr.js
@@ -686,11 +686,13 @@ function Flatpickr(element, config) {
 
 		else if (e.target.classList.contains("flatpickr-day") && !e.target.classList.contains("disabled")) {
 			const isPrevMonthDay = e.target.classList.contains("prevMonthDay"),
-				isNextMonthDay = e.target.classList.contains("nextMonthDay"),
-				monthNum = self.currentMonth - isPrevMonthDay + isNextMonthDay;
+				isNextMonthDay = e.target.classList.contains("nextMonthDay");
+			let monthNum = self.currentMonth - isPrevMonthDay + isNextMonthDay;
 
-			if (isPrevMonthDay || isNextMonthDay)
+			if (isPrevMonthDay || isNextMonthDay) {
 				changeMonth(+isNextMonthDay - isPrevMonthDay);
+				monthNum = self.currentMonth;
+			}
 
 			self.selectedDateObj = new Date(self.currentYear, monthNum, e.target.innerHTML);
 
